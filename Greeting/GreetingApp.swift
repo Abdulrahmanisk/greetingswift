00d4d006b7fe7e48f4e705fb55922d9b42466a8d
Abdulrahmanisk <Primary.isk@gmail.com>
2025-01-17 07:38:54 +0300

    11

---
+++ Greeting/GreetingApp.swift
@@ -0,0 +1,17 @@
+//
+//  GreetingApp.swift
+//  Greeting
+//
+//  Created by Abdulrahman isk on 17/07/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct GreetingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
